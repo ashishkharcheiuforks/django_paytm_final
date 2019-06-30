ac686f2583b57037dc62d382f5eb71036586a97d
iudragon <iudragon32@gmail.com>
2019-06-30 14:32:19 +0530

    master

--- deploytodotasker/settings.py
+++ deploytodotasker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
-#import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +25,9 @@ SECRET_KEY = '-jg2ngl7$$ejo!&cr7v^#yxcyggdo#bm4!op3x3m72nnf5#1l0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'rafi9z.pythonanywhere.com',
+    ]
 
 
 # Application definition
@@ -126,13 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 LOGIN_REDIRECT_URL = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -170,24 +169,14 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details',
 )
-
-##################For payTM payment################################
-## Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+#paytm mid: Ulbgcl83114033677105 paytm test secret key: ca22yp3gV!0d&%L8
 PAYTM_MERCHANT_KEY = "ca22yp3gV!0d&%L8"
 PAYTM_MERCHANT_ID = "Ulbgcl83114033677105"
 PAYTM_WEBSITE = 'WEB_STAGING'
-CALLBACK_URL="https://securegw.paytm.in/theia/paytmCallback?ORDER_ID="
-CHANNEL_ID='WEB'
-WEBSITE='WEBSTAGING'
-INDUSTRY_TYPE_ID='Retail'
-## for staging## see at : https://developer.paytm.com/docs/transaction-status-api
-VERIFY_URL="https://securegw-stage.paytm.in/order/status"
-## for production
-#VERIFY_URL="https://securegw.paytm.in/order/status" 
-######################################################################
+
+STRIPE_API_KEY = 'sk_test_lOwRe5hiXBypwwU6x2PPgXmj'
+
 try:
     from.local_settings import *
 except ImportError:
     pass
-
-
