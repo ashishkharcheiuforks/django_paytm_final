ebdaad12d075bb239023b03f924dd3b4d15bab69
iudragon <iudragon32@gmail.com>
2019-07-10 01:11:36 +0530

    ds

--- deploytodotaskerapp/models.py
+++ deploytodotaskerapp/models.py
@@ -111,7 +111,7 @@ class PaytmHistory(models.Model):
     # CURRENCY = models.CharField('CURRENCY', max_length=4, null=True, blank=True)
 
     def __str__(self):
-        return str(self.order + "hello" + self.customer.user.get_full_name)
+        return str(self.order + "hello" + self.customer.hobby)
 
     def __unicode__(self):
         return self.STATUS
