372c0ae22b6069a67eab28d66e77b87bb5c69fec
iudragon <iudragon32@gmail.com>
2019-07-11 11:51:28 +0530

    ds

--- deploytodotaskerapp/models.py
+++ deploytodotaskerapp/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.conf import settings
+
 # Create your models here.
 class Registration(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='registration')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="registration"
+    )
     name = models.CharField(max_length=500)
     phone = models.CharField(max_length=500)
     address = models.CharField(max_length=500)
-    logo = models.ImageField(upload_to='registration_logo/', blank=False)
+    logo = models.ImageField(upload_to="registration_logo/", blank=False)
 
     def __str__(self):
         return self.name
 
+
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="customer")
     avatar = models.CharField(max_length=500)
     phone = models.CharField(max_length=500, blank=True)
     address = models.CharField(max_length=500, blank=True)
@@ -22,8 +26,9 @@ class Customer(models.Model):
     def __str__(self):
         return self.user.get_full_name()
 
+
 class Driver(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='driver')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="driver")
     avatar = models.CharField(max_length=500)
     phone = models.CharField(max_length=500, blank=True)
     address = models.CharField(max_length=500, blank=True)
@@ -32,15 +37,28 @@ class Driver(models.Model):
     def __str__(self):
         return self.user.get_full_name()
 
+
 class Meal(models.Model):
     registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
     name = models.CharField(max_length=500)
     short_description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='meal_images/', blank=False)
+    image = models.ImageField(upload_to="meal_images/", blank=False)
     price = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
+
+
+class Drink(models.Model):
+    registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)
+    short_description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to="drink_images/", blank=False)
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     COOKING = 1
@@ -57,57 +75,59 @@ class Order(models.Model):
 
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
-    driver = models.ForeignKey(Driver, blank = True, null = True, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, blank=True, null=True, on_delete=models.CASCADE)
     address = models.CharField(max_length=500)
     total = models.IntegerField()
-    status = models.IntegerField(choices = STATUS_CHOICES)
-    created_at = models.DateTimeField(default = timezone.now)
-    picked_at = models.DateTimeField(blank = True, null = True)
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+    picked_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return str(self.id)
 
 
 class OrderDetails(models.Model):
-    order = models.ForeignKey(Order, related_name='order_details', on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, related_name="order_details", on_delete=models.CASCADE
+    )
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     sub_total = models.IntegerField()
 
     def __str__(self):
         return str(self.id)
 
+
 class PaytmHistory(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, related_name='order_payment', on_delete=models.CASCADE)
-    TXNID = models.CharField('TNX ID', max_length=70)
-    BANKTXNID = models.CharField('BANK TXN ID', max_length=60, null=True, blank=True)
-    ORDERID = models.CharField('ORDER ID', max_length=70)
-    TXNAMOUNT = models.FloatField('TXN AMOUNT')
-    STATUS = models.CharField('STATUS', max_length=30)
-    TXNTYPE=models.CharField('TNX TYPE', max_length=10, null=True, blank=True)
+    order = models.ForeignKey(
+        Order, related_name="order_payment", on_delete=models.CASCADE
+    )
+    TXNID = models.CharField("TNX ID", max_length=70)
+    BANKTXNID = models.CharField("BANK TXN ID", max_length=60, null=True, blank=True)
+    ORDERID = models.CharField("ORDER ID", max_length=70)
+    TXNAMOUNT = models.FloatField("TXN AMOUNT")
+    STATUS = models.CharField("STATUS", max_length=30)
+    TXNTYPE = models.CharField("TNX TYPE", max_length=10, null=True, blank=True)
     GATEWAYNAME = models.CharField("GATEWAY NAME", max_length=20, null=True, blank=True)
-    RESPCODE = models.CharField('STATUS', max_length=20)
-    RESPMSG = models.TextField('RESP MSG', max_length=600)
-    BANKNAME = models.CharField('BANK NAME', max_length=600, null=True, blank=True)
+    RESPCODE = models.CharField("STATUS", max_length=20)
+    RESPMSG = models.TextField("RESP MSG", max_length=600)
+    BANKNAME = models.CharField("BANK NAME", max_length=600, null=True, blank=True)
     MID = models.CharField(max_length=40)
-    PAYMENTMODE = models.CharField('PAYMENT MODE', max_length=20, null=True, blank=True)
-    REFUNDAMT = models.FloatField('RFUND AMOUNT',default=0)
-    TXNDATE = models.DateTimeField('TXN DATE', default=timezone.now)
-    #CURRENCY = models.CharField('CURRENCY', max_length=4, null=True, blank=True)
+    PAYMENTMODE = models.CharField("PAYMENT MODE", max_length=20, null=True, blank=True)
+    REFUNDAMT = models.FloatField("RFUND AMOUNT", default=0)
+    TXNDATE = models.DateTimeField("TXN DATE", default=timezone.now)
+    # CURRENCY = models.CharField('CURRENCY', max_length=4, null=True, blank=True)
 
     def __unicode__(self):
         return self.STATUS
 
 
 class ImageStore(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='image')
-    image = models.ImageField(upload_to='images/%Y/%m/%d')#upload_to='blog/%Y/%m/%d'
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="image")
+    image = models.ImageField(upload_to="images/%Y/%m/%d")  # upload_to='blog/%Y/%m/%d'
+
     def __str__(self):
         return self.user.get_full_name()
-
-
-
-
-
 
--- deploytodotaskerapp/serializers.py
+++ deploytodotaskerapp/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
 
-from deploytodotaskerapp.models import Registration, \
-    Meal, \
-    Customer, \
-    Driver, \
-    Order, \
-    OrderDetails
+from deploytodotaskerapp.models import (
+    Registration,
+    Meal,
+    Drink,
+    Customer,
+    Driver,
+    Order,
+    OrderDetails,
+)
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     logo = serializers.SerializerMethodField()
 
     def get_logo(self, registration):
-        request = self.context.get('request')
+        request = self.context.get("request")
         logo_url = registration.logo.url
         return request.build_absolute_uri(logo_url)
 
@@ -25,12 +28,25 @@ class MealSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
     def get_image(self, meal):
-        request = self.context.get('request')
+        request = self.context.get("request")
         image_url = meal.image.url
         return request.build_absolute_uri(image_url)
 
     class Meta:
         model = Meal
+        fields = ("id", "name", "short_description", "image", "price")
+
+
+class DrinkSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, drink):
+        request = self.context.get("request")
+        image_url = drink.image.url
+        return request.build_absolute_uri(image_url)
+
+    class Meta:
+        model = Drink
         fields = ("id", "name", "short_description", "image", "price")
 
 
@@ -62,13 +78,15 @@ class OrderMealSerializer(serializers.ModelSerializer):
         model = Meal
         fields = ("id", "name", "price")
 
+class OrderDrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ("id", "name", "price")
+
 
 class OrderDetailsSerializer(serializers.ModelSerializer):
     meal = OrderMealSerializer()
-
-    class Meta:
-        model = OrderDetails
-        fields = ("id", "meal", "quantity", "sub_total")
+    drink = OrderDrinkSerializer()
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -80,4 +98,13 @@ class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ("id", "customer", "registration", "driver", "order_details", "total", "status", "address")
+        fields = (
+            "id",
+            "customer",
+            "registration",
+            "driver",
+            "order_details",
+            "total",
+            "status",
+            "address",
+        )
